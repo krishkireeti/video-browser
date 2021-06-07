99d9c897b57e250bf2c43ff4db8a3eb6ff2f7122
Kirruu <kirikom94@gmail.com>
2021-06-07 11:04:33 +0530

    Latest Integration with Vue Analytics and readme

---
+++ readme.md
@@ -0,0 +1,4 @@
+# video-browser
+
+In this project i have used Youtube API v3 and you can search in the search bar and can play the video.
+This project mainly uses Vue and its concepts
--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue';
 import App from './App';
+import VueAnalytics from 'vue-analytics';
+
+Vue.use(VueAnalytics, {
+  id: 'UA-93238037-2'
+});
 
 new Vue({
   render: h => h(App)
